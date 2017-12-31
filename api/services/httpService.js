51b8d8ec3b77044ec57c01d041fd883e01084269
Diogo Augusto Pereira <diogoap82@gmail.com>
2017-12-31 15:46:25 -0200

    headers and support parameters at query string

--- api/services/httpService.js
+++ api/services/httpService.js
@@ -3,16 +3,16 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var getHttpDataYahoo = function(ticker, callbackSuccess, callbackError) {
+var getHttpDataYahoo = function (ticker, callbackSuccess, callbackError) {
 	var url = 'https://finance.yahoo.com/quote/' + ticker;
 
-	request(url, function(error, response, html){
-		if(!error){
+	request(url, function (error, response, html) {
+		if (!error) {
 			var $ = cheerio.load(html);
 
 			console.log('	getHttpData - Loaded for ' + ticker);
 
-			$('.Trsdu\\(0\\.3s\\).Fw\\(b\\).Fz\\(36px\\).Mb\\(-4px\\).D\\(ib\\)').filter(function(){
+			$('.Trsdu\\(0\\.3s\\).Fw\\(b\\).Fz\\(36px\\).Mb\\(-4px\\).D\\(ib\\)').filter(function () {
 				var data = $(this);
 				callbackSuccess(ticker, data.text());
 			})
@@ -22,24 +22,28 @@ var getHttpDataYahoo = function(ticker, callbackSuccess, callbackError) {
 	})
 }
 
-var getHttpDataInvesting = function(ticker, callbackSuccess, callbackError) {
-	var parsedTicker = ticker.replace('_', '/');
+var getHttpDataInvesting = function (ticker, callbackSuccess, callbackError) {
+	var parsedTicker = ticker.replace('_', '/').replace('!','?');
 	var url = 'https://m.br.investing.com/' + parsedTicker;
 
-	console.log(url);
-
-	request(url, function(error, response, html){
-		if(!error){
+	request({
+		url: url,
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+		}
+	},
+	function (error, response, html) {
+		if (!error && response.statusCode == 200) {
 			var $ = cheerio.load(html);
 
 			console.log('	getHttpDataInvesting - Loaded for ' + ticker);
 
-			$('.quotesBoxTop').filter(function(){
+			$('.quotesBoxTop').filter(function () {
 				var data = $(this).find('span').first();
 				callbackSuccess(ticker, data.text());
-			})			
+			})
 		} else {
-			callbackError(error, 404);
+			callbackError(error, response.statusCode);
 		}
 	})
 }
