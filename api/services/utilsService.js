33be76cf87390b76573603b87df2df67e45d0309
Diogo Augusto Pereira <diogoap82@gmail.com>
2020-03-12 19:42:59 +0100

    Add logging

--- api/services/utilsService.js
+++ api/services/utilsService.js
@@ -2,6 +2,10 @@
 
 var sendError = function(res, error, status) {
 	if (status) {
+		console.log('Error on the request. Message');
+		console.log(error);	
+		console.log('Details:');			
+		console.log(res);
 		res.status(status).end('Error: ' + error);
 	} else {
 		res.status(500).end(error);
