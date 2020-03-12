218ae520e1402ded59aeb4a7b3a51ffdaa43aa3d
Diogo Augusto Pereira <diogoap82@gmail.com>
2020-03-12 20:22:03 +0100

    More logs

--- api/services/httpService.js
+++ api/services/httpService.js
@@ -31,7 +31,7 @@ var getHttpDataInvesting = function (ticker, callbackSuccess, callbackError) {
 		headers: {
 			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
 		}
-	},
+	},{},
 	function (error, response, html) {
 		if (!error && response.statusCode == 200) {
 			var $ = cheerio.load(html);
