b43ddbc3d9b2bab153304e3122fd797e270d5de0
Diogo Augusto Pereira <diogoap82@gmail.com>
2020-03-12 21:40:18 +0100

    Replace request

--- api/services/httpService.js
+++ api/services/httpService.js
@@ -1,56 +1,53 @@
 'use strict';
 
-var request = require('request');
-var cheerio = require('cheerio');
-
-var getHttpDataYahoo = function (ticker, callbackSuccess, callbackError) {
-	var url = 'https://finance.yahoo.com/quote/' + ticker;
-
-	request(url, function (error, response, html) {
-		if (!error) {
-			var $ = cheerio.load(html);
-
-			console.log('	getHttpData - Loaded for ' + ticker);
-
-			$('.Trsdu\\(0\\.3s\\).Fw\\(b\\).Fz\\(36px\\).Mb\\(-4px\\).D\\(ib\\)').filter(function () {
-				var data = $(this);
-				callbackSuccess(ticker, data.text());
-			})
-		} else {
-			callbackError(error, 404);
-		}
-	})
-}
+const https = require('https')
+const cheerio = require('cheerio');
 
 var getHttpDataInvesting = function (ticker, callbackSuccess, callbackError) {
-	var parsedTicker = ticker.replace('_', '/').replace('!','?');
-	var url = 'https://m.br.investing.com/' + parsedTicker;
+	var parsedTicker = ticker.replace('_', '/').replace('!', '?');
 
-	request({
-		url: url,
+	const options = {
+		hostname: 'm.br.investing.com',
+		port: 443,
+		path: `/${parsedTicker}`,
+		method: 'GET',
 		headers: {
 			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
 		}
-	},{},
-	function (error, response, html) {
-		if (!error && response.statusCode == 200) {
-			var $ = cheerio.load(html);
+	}
 
-			console.log('	getHttpDataInvesting - Loaded for ' + ticker);
-			
-			$('.quotesBoxTop').filter(function () {
-				var data = $(this).find('span').first();
-				callbackSuccess(ticker, data.text().replace(/[\r\n|\s] /g,''));
-			})
-		} else {
-			console.log('Eror HTML');
-			console.log(html);
-			callbackError(error, response.statusCode);
-		}
+	const req = https.request(options, res => {
+		console.log(`Request for ${ticker} completed. StatusCode: ${res.statusCode}`)
+
+		res.on('data', html => {
+
+			if (res.statusCode == 200)
+			{
+				var $ = cheerio.load(html);
+	
+				console.log(`** OK ** getHttpDataInvesting - Loaded for ${ticker}`);
+	
+				$('.quotesBoxTop').filter(function () {
+					var data = $(this).find('span').first();
+					callbackSuccess(ticker, data.text().replace(/[\r\n|\s] /g, ''));
+				})
+			} else {
+				console.log(`Request result for ${ticker} was not successful. Resquest options:`)								
+				console.log(options)
+				callbackError('HTTP result request was not successful', res.statusCode);				
+			}
+		})
 	})
+
+	req.on('error', error => {
+		console.log('HTTP request failed:');
+		console.log(error);
+		callbackError(error, 500)
+	})
+
+	req.end()
 }
 
 module.exports = {
-	getHttpDataYahoo: getHttpDataYahoo,
 	getHttpDataInvesting: getHttpDataInvesting
 }
